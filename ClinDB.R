ee5059d325e1252099ff037a81c3a68ee3e0e775
MountainMan12 <pawan12394@gmail.com>
2020-07-14 20:57:58 +0530

    Update 1 : Generating Clinical Database for Cancer

---
+++ ClinDB.R
@@ -0,0 +1,29 @@
+library(data.table)
+library(TCGAbiolinks)
+library(regexPipes)
+library(tidyverse)
+library(RSQLite)
+
+project_id <- TCGAbiolinks::getGDCprojects()$project_id %>%
+  regexPipes::grep("TCGA", value = T) 
+
+clinical <- vector(mode = "list", length = length(project_id))
+j <- 1
+for (i in project_id) {
+  clinical[[j]] <- GDCquery_clinic(project = i, type = "clinical")
+  j <- j + 1
+}
+
+#Create an sqllite database 
+db <- src_sqlite("ClinicalDb.sqlite", create = TRUE)
+k <- seq(1, length(project_id), by = 1)
+for (i in k) {
+  copy_to(db, df = clinical[[i]], name = project_id[[i]], indexes = list(colnames(clinical[[i]])), temporary = FALSE, overwrite = TRUE)
+}
+
+# View the tables
+sqlite <- dbDriver("SQLite")
+clin_tables <- dbConnect(sqlite, "ClinicalDb.sqlite")
+dbListTables(clin_tables)
+#TCGA_BLCA <- tbl(db, "TCGA-BLCA")
+#head(collect(TCGA_BLCA))
